bda1824da4ebfb8af5d460c786321e9fffb91f56
Mai <garcia.ton94@gmail.com>
2023-09-24 21:00:38 -0400

    getJobsByCompanyId

--- pages/api/test-strapi.js
+++ pages/api/test-strapi.js
@@ -1,3 +1,4 @@
+import { data } from "autoprefixer";
 import datasource from "../../datalayer";
 
 export default async function handler(req, res) {
@@ -7,9 +8,10 @@ export default async function handler(req, res) {
 
   //   const data = await datasource.getJobsSlugs();
 
-  const data = await datasource.getJobBySlug({
-    slug: "frontend-expert-tailwind-css-guru",
-  });
+  //   const data = await datasource.getJobBySlug({
+  //     slug: "frontend-expert-tailwind-css-guru",
+  //   });
 
+  const data = await datasource.getJobsByCompanyId({ id: 1 });
   res.status(200).json(data);
 }
