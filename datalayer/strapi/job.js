0c6b061034154e36bebb306044af0ce0dee23ae0
Mai <garcia.ton94@gmail.com>
2023-09-28 21:34:55 -0400

    lesson 21

--- datalayer/strapi/job.js
+++ datalayer/strapi/job.js
@@ -3,8 +3,22 @@ import qs from "qs";
 
 const apiUrl = process.env.STRAPI_API_BASE_URL;
 
-export const getJobs = async () => {
-  const res = await axios.get(`${apiUrl}/jobs`);
+export const getJobs = async ({ page = 0, pageSize = 25 } = {}) => {
+  const query = qs.stringify(
+    {
+      populate: ["company", "company.logo", "skillTags"],
+      pagination: {
+        // start,
+        // limit,
+        page,
+        pageSize,
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  const res = await axios.get(`${apiUrl}/jobs?${query}`);
   const rawJobs = res.data.data;
   return rawJobs;
 };
@@ -62,7 +76,7 @@ export const getJobsByCompanyId = async ({ id }) => {
           },
         },
       },
-      populate: ["company", "company.logo", "skillsTags"],
+      populate: ["company", "company.logo", "skillTags"],
     },
     {
       encodeValuesOnly: true,
@@ -70,6 +84,76 @@ export const getJobsByCompanyId = async ({ id }) => {
   );
 
   const res = await axios.get(`${apiUrl}/jobs?${query}`);
+  const rawJobs = res.data.data;
+  return rawJobs;
+};
+
+export const searchJobs = async (query) => {
+  const strapiQuery = {
+    populate: ["company", "company.logo", "skillTags"],
+    filters: {},
+  };
+
+  // Add Boolean Query Filters
+  if (query.remoteOkOnly) strapiQuery["filters"]["remoteOK"] = { $eq: true };
+  if (query.featuredJobsOnly)
+    strapiQuery["filters"]["featuredJob"] = { $eq: true };
+
+  // Add Range Query Filters
+  strapiQuery["filters"]["baseAnnualSalary"] = {
+    $gte: query.minBaseSalary,
+    $lte: query.maxBaseSalary,
+  };
+
+  // Add Inclusion Query Filters
+  if (query.jobTypes && query.jobTypes.length) {
+    strapiQuery["filters"]["jobType"] = { $in: query.jobTypes };
+  }
+
+  if (query.experienceLevels && query.experienceLevels.length) {
+    strapiQuery["filters"]["experienceLevel"] = { $in: query.experienceLevels };
+  }
+
+  // Add Nested Inclusion Query Filters
+  if (query.selectedTags && query.selectedTags.length) {
+    strapiQuery["filters"]["skillTags"] = {
+      name: { $in: query.selectedTags },
+    };
+  }
+
+  // Add Full Text Search
+  if (query.searchBarText) {
+    const searchFields = [
+      "title",
+      "jobCategory",
+      "jobType",
+      "jobDescription",
+      "aboutYou",
+      "jobResponsibilities",
+      "remunerationPackage",
+      // deep nested search fields
+      "skillTags.name",
+      "company.name",
+      "company.city",
+    ];
+
+    strapiQuery["filters"]["$or"] = searchFields.map((field) => {
+      const searchField = {};
+
+      if (!field.includes(".")) {
+        searchField[field] = { $containsi: query.searchBarText };
+      } else {
+        const [level1, level2] = field.split(".");
+        const nestedSearchField = {};
+        nestedSearchField[level2] = { $containsi: query.searchBarText };
+        searchField[level1] = nestedSearchField;
+      }
+      return searchField;
+    });
+  }
+
+  const strapiQueryStr = qs.stringify(strapiQuery, { encodeValuesOnly: true });
+  const res = await axios.get(`${apiUrl}/jobs?${strapiQueryStr}`);
   const rawJobs = res.data.data;
   return rawJobs;
 };
--- pages/api/test-strapi.js
+++ pages/api/test-strapi.js
@@ -1,4 +1,3 @@
-import { data } from "autoprefixer";
 import datasource from "../../datalayer";
 
 export default async function handler(req, res) {
@@ -12,6 +11,30 @@ export default async function handler(req, res) {
   //     slug: "frontend-expert-tailwind-css-guru",
   //   });
 
-  const data = await datasource.getJobsByCompanyId({ id: 1 });
+  // const data = await datasource.getJobsByCompanyId({ id: 1 });
+
+  // const data = await datasource.getJobs({ page: 1, pageSize: 2 });
+  // const data = await datasource.searchJobs({ remoteOkOnly: true });
+
+  // const data = await datasource.searchJobs({
+  //   minBaseSalary: 90000,
+  //   maxBaseSalary: 120000,
+  // });
+
+  // const data = await datasource.searchJobs({
+  //   jobTypes: ["Internship", "Contract"],
+  // });
+
+  // const data = await datasource.searchJobs({
+  //   experienceLevels: ["Senior", "Tech-Lead"],
+  // });
+
+  // const data = await datasource.searchJobs({
+  //   selectedTags: ["html5"],
+  // });
+
+  const data = await datasource.searchJobs({
+    searchBarText: "London",
+  });
   res.status(200).json(data);
 }
